8c8d5c9fd2d46ed7173622434e6ffc38ad1ced8a
Alejandro Cordero <a.cordero@students.chapelfield.org>
2019-03-21 09:44:29 -0400

    Read on and off correctly

--- arduino/blink/serialBlink/serialBlink.ino
+++ arduino/blink/serialBlink/serialBlink.ino
@@ -1,20 +1,27 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as
-  Serial.begin(9600); 
+  Serial.begin(9600);
+  Serial.setTimeout(10);
   Serial.println("love you");
-  Serial.println("send 'on' to turn on the LED.");
-  Serial.println("send 'off' to turn off the LED.");
+  Serial.println("send 'On' to turn On the LED.");
+  Serial.println("send 'Off' to turn Off the LED.");
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  Serial.println("on");
-  delay(1000);
-  digitalWrite(13,LOW);
-  Serial.println("off");
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  if (Serial.available() > 0) {
+    String command;
+    command = Serial.readString();
+    command.trim();
+    if (command == "On") {
+      digitalWrite(13, HIGH);
+      Serial.println("On");
+    }
+    else if (command == "Off") {
+      digitalWrite(13, LOW);
+      Serial.println("Off");
+    }
+  }
+
 }
