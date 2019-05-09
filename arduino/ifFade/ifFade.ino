01ecf53921b07dd53bb21aa2800b04fe3980f7c0
Alejandro Cordero <a.cordero@students.chapelfield.org>
2019-05-09 11:42:55 -0400

    add a flip

--- arduino/ifFade/ifFade.ino
+++ arduino/ifFade/ifFade.ino
@@ -1,10 +1,14 @@
 int lightValue = 0;
 int delta = 1;
 void setup() {
- Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(9, OUTPUT);
 
 }
 
 void loop() {
+  lightValue = lightValue + delta;
+  if (lightValue <= 0 || lightValue >= 255) {
+    delta = -delta;
+  }
 }
