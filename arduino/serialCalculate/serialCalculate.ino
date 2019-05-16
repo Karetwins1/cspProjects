eee6057632463fca267b5a922c31e40e46605152
Alejandro Cordero <a.cordero@students.chapelfield.org>
2019-05-16 11:39:41 -0400

    Will sanitizing data

--- arduino/serialCalculate/serialCalculate.ino
+++ arduino/serialCalculate/serialCalculate.ino
@@ -1,11 +1,19 @@
-  int number1;
-  char sign;
-  int number2;
-  int result;
+int number1;
+char sign;
+int number2;
+int result;
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-
+  cleanData();
+}
+void cleanData() {
+  while (Serial.available() > 0) {
+    char inbound = Serial.read();
+    if (isGraph(inbound)) {
+      Serial.println(inbound);
+    }
+  }
 }
